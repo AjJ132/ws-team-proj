b75412aceefb1503779739343a3b7cbd2c0cdd55
AJ Johnson <54604582+AjJ132@users.noreply.github.com>
2025-04-23 18:41:30 -0400

    refactor: simplify layout component; rename DashboardLayout to Layout and clean up unused code

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,24 +1,13 @@
-
-
 import SimpleNavbar from "@/components/navbar/simple-navbar";
 import "../globals.css";
 
-
-
-export async function DashboardLayout({ children,}: Readonly<{ children: React.ReactNode; }>) {
-
-
+export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
       <SimpleNavbar />
-
       <div className="max-w-screen h-full w-full overflow-hidden flex flex-col px-0 pt-4 lg:pt-16 bg-background2 min-h-screen">
-        
         {children}
-
       </div>
     </>
-  )
+  );
 }
-
-export default DashboardLayout;
